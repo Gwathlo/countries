5461721e194cf9cd9983c503059bd88de17dc9cb
Gwathlo <gwath@yahoo.com>
2021-03-28 22:38:03 +0200

    For region buttons

--- src/app/country/pages/for-region/for-region.component.ts
+++ src/app/country/pages/for-region/for-region.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-for-region',
   templateUrl: './for-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
-export class ForRegionComponent implements OnInit {
+export class ForRegionComponent {
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activatedRegion: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  getClass(region: string): string {
+    return region === this.activatedRegion
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
+  activeRegion(region: string) {
+    this.activatedRegion = region;
+
+    // TODO service call
+  }
 }
